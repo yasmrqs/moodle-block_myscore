12c604c606385a56860c7a9bf4e227e2f75c7ca2
Yasmin Marques <yasmin.teotonio@usp.com>
2023-02-08 17:31:16 -0300

    alterações na view

--- view.php
+++ view.php
@@ -5,30 +5,44 @@ require_once('../../config.php');
 global $DB;
 global $USER; 
 
-
-$resultados = $DB->get_records_sql('select grade from mdl_assign_grades');
-
 echo "<pre>";
 
-$valores = array_column($resultados, 'grade');
-              
-function desempenho($valores){
-    foreach($valores as $n) {
-            echo "\n\n" . "Atenção o seu desempenho teve uma ";
-        if ($n <= 40.00000) {
-            echo 'nota baixa, você precisa estudar mais!';
-        }
-        elseif ($n >= 50.00000 && $n <=70.00000) {
-            echo 'nota média, você pode melhorar mais!';
-        }
-        elseif ($n >= 80.00000) {
-            echo 'nota alta, continue assim!';
-        }
-    }
+//a variável possui a média final de todas as atividades entregues pelos alunos
+$resultados = $DB->get_records_sql("SELECT id, userid, rawgrade, AVG(rawgrade) AS MediaFinal FROM 'mdl_grade_grades' GROUP BY userid;");
+$valores = array_column($resultados, 'mediafinal');
+
+//die();
+
+$nomes = $DB->get_records_sql('select id, firstname, lastname from mdl_user');
+$id = array_column($nomes, 'id');
+$pn = array_column($nomes, 'firstname');
+$sn = array_column($nomes, 'lastname');
+
+$cont = 1;
+$contro = 0; 
+
+for ($controlador == 0; $cont != NULL;){
+
+    echo("\n\n");
+
+if ($valores[$contro] <= 4 && $valores[contro] != NULL){
+    echo $pn[$controlador] . ' ' . $sn[$controlador] . "O seu desempenho foi " . $valores[$contro] . ". A sua nota foi baixa, portanto estude mais!";
+    $contro++;
 }
 
-echo desempenho($valores);
+elseif ($valores[$contro] >= 5 && $valores[contro] <= 7 && $valores[$contro] != NULL){
+    echo $pn[$controlador] . ' ' . $sn[$controlador] . "O seu desempenho foi " . $valores[$contro] . ". A sua nota foi mediana, você pode melhorar!";
+    $contro++;
+}
 
+elseif ($valores[$contro] >= 8 && $valores[contro] != NULL){
+    echo $pn[$controlador] . ' ' . $sn[$controlador] . "O seu desempenho foi " . $valores[$contro] . ". Parabéns! A sua nota foi mediana, continue assim!";
+    $contro++;
+}
 
+$cont = $pn[$controlador];
+$controlador++;
+
+}
 
 
